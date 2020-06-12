b9aa5d4f653272b61b55d1407239d88347287221
cdelga05 <63617594+cdelga05@users.noreply.github.com>
2020-06-12 09:08:01 -0700

    Update form.php

--- form.php
+++ form.php
@@ -1,13 +1,12 @@
 <?php
-if (){}else{//show data
-
+if (isset($_GET["FirstName"])){//show data
+    echo "FirstName";
 }else{//show form 
    echo '
    <form action="">
-   
+   <p>First Name: <input type="text" name="FirstName" /></p>
+   <p><input type="submit"/></p>
    </form>
    ';
-
-
 }
 ?>
